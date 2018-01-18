bdfe44088a152d1f45dd1f37c53db7fa98d2e517
David Dos Santos <david.dossantos@datadoghq.com>
2018-01-18 16:10:30 +0100

    refactor language.ml

--- src/language.ml
+++ src/language.ml
@@ -2,8 +2,6 @@
  * Language definition
  *)
 
-module SS = Set.Make(String)
-
 type language = Skip    of string
               | Assign  of string   * string          * Aexp.arith_expr
               | Compose of language * language
@@ -11,54 +9,76 @@ type language = Skip    of string
               | While   of string   * Bexp.bool_expr  * language
               ;;
 
+
+module SS = Set.Make(String)
+
 (*
- * Find labels of a program for a given execution
- * identified by an initial state
+ * Run a program
+ * and collect labels hit during the execution
  *)
-let rec run_and_collect_labels state language =
-    match language with
-    | Skip (label) -> SS.singleton label
-    | Assign (label, variable, expr) ->
-        Hashtbl.replace state variable ( Aexp.eval state expr );
-        SS.singleton label;
+
+let rec run_and_collect_labels label_filter state program =
+    match program with
+    | Skip (l) as c ->
+        label_filter c
+    | Assign (l, var, aexp) as c ->
+        Hashtbl.replace state var (Aexp.eval state aexp);
+        label_filter c
     | Compose (c1, c2) ->
-        SS.union (run_and_collect_labels state c1) (run_and_collect_labels state c2)
-    | If (label, cond, c1, c2) ->
-        let labels =
-            match ( Bexp.eval state cond) with
-            | true  -> run_and_collect_labels state c1
-            | false -> run_and_collect_labels state c2
-        in SS.add label labels
-    | While (label, cond, c) ->
-        let new_labels cond c =
-            match ( Bexp.eval state cond) with
-            | true ->
-                SS.union (run_and_collect_labels state c) (run_and_collect_labels state (While(label, cond, c)))
-            | false ->
-                SS.empty
-        in SS.add label (new_labels cond c)
+        let s1 = run_and_collect_labels label_filter state c1
+        and s2 = run_and_collect_labels label_filter state c2
+        in SS.union s1 s2
+    | If (l, bexp, c1, c2) as c ->
+        let s =
+        match Bexp.eval state bexp with
+        | true  -> run_and_collect_labels label_filter state c1
+        | false -> run_and_collect_labels label_filter state c2
+        in SS.union (label_filter c) s
+    | While (l, bexp, c1) as c ->
+        let s =
+        match Bexp.eval state bexp with
+        | true ->
+            SS.union
+                (run_and_collect_labels label_filter state c1)
+                (run_and_collect_labels label_filter state c)
+        | false -> SS.empty
+        in SS.union (label_filter c) s
 
-let run state language =
-    run_and_collect_labels state language;
-    ();;
 
 (*
- * Find all the labels of a program
+ * Collect labels in the whole program
  *)
-let rec collect_labels language =
-    match language with
-    | Skip    (label)            -> SS.singleton label
-    | Assign  (label, _, _)      -> SS.singleton label
-    | Compose (c1, c2)           ->
-        let s1 = collect_labels c1
-        and s2 = collect_labels c2
-        in  SS.union s1 s2
-    | If      (label, _, c1, c2) ->
-        let s1 = collect_labels c1
-        and s2 = collect_labels c2
-        in let s = SS.union s1 s2
-        in SS.add label s
-    | While   (label, _, c)      ->
-        let s = collect_labels c
-        in SS.add label s
+let rec collect_labels label_filter program =
+    match program with
+    | Skip (l) as c ->
+        label_filter c
+    | Assign (_, _, _) as c ->
+        label_filter c
+    | Compose (c1, c2) ->
+        let s1 = collect_labels label_filter c1
+        and s2 = collect_labels label_filter c2
+        in SS.union s1 s2
+    | If (l, _, c1, c2) as c ->
+        let s1 = collect_labels label_filter c1
+        and s2 = collect_labels label_filter c2
+        and s3 = label_filter c
+        in
+        SS.union ( SS.union s1 s2) s3
+    | While (l, _, c1) as c ->
+        let s1 = collect_labels label_filter c1
+        in SS.union s1 (label_filter c)
+
+(*
+ * Execute a program
+ *)
+let rec run =
+    let dummy_label_filter c = SS.empty
+    in run_and_collect_labels dummy_label_filter
+
+let label_filter_none program =
+    match program with
+    | Skip (l) -> SS.singleton l
+    | Assign(l, _, _) -> SS.singleton l
+    | If (l, _, _, _) -> SS.singleton l
+    | While (l, _, _) -> SS.singleton l
     ;;
