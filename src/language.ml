4b4205f8119fd458e8cb8361b19bc8ebc690846a
David Dos Santos <david.dossantos@datadoghq.com>
2018-01-18 19:01:21 +0100

    add modulo and refactor

--- src/language.ml
+++ src/language.ml
@@ -10,75 +10,30 @@ type language = Skip    of string
               ;;
 
 
-module SS = Set.Make(String)
-
-(*
- * Run a program
- * and collect labels hit during the execution
- *)
-
-let rec run_and_collect_labels label_filter state program =
-    match program with
-    | Skip (l) as c ->
-        label_filter c
-    | Assign (l, var, aexp) as c ->
-        Hashtbl.replace state var (Aexp.eval state aexp);
-        label_filter c
-    | Compose (c1, c2) ->
-        let s1 = run_and_collect_labels label_filter state c1
-        and s2 = run_and_collect_labels label_filter state c2
-        in SS.union s1 s2
-    | If (l, bexp, c1, c2) as c ->
-        let s =
-        match Bexp.eval state bexp with
-        | true  -> run_and_collect_labels label_filter state c1
-        | false -> run_and_collect_labels label_filter state c2
-        in SS.union (label_filter c) s
-    | While (l, bexp, c1) as c ->
-        let s =
-        match Bexp.eval state bexp with
-        | true ->
-            SS.union
-                (run_and_collect_labels label_filter state c1)
-                (run_and_collect_labels label_filter state c)
-        | false -> SS.empty
-        in SS.union (label_filter c) s
-
-
-(*
- * Collect labels in the whole program
- *)
-let rec collect_labels label_filter program =
-    match program with
-    | Skip (l) as c ->
-        label_filter c
-    | Assign (_, _, _) as c ->
-        label_filter c
-    | Compose (c1, c2) ->
-        let s1 = collect_labels label_filter c1
-        and s2 = collect_labels label_filter c2
-        in SS.union s1 s2
-    | If (l, _, c1, c2) as c ->
-        let s1 = collect_labels label_filter c1
-        and s2 = collect_labels label_filter c2
-        and s3 = label_filter c
-        in
-        SS.union ( SS.union s1 s2) s3
-    | While (l, _, c1) as c ->
-        let s1 = collect_labels label_filter c1
-        in SS.union s1 (label_filter c)
-
 (*
  * Execute a program
  *)
-let rec run =
-    let dummy_label_filter c = SS.empty
-    in run_and_collect_labels dummy_label_filter
-
-let label_filter_none program =
+let rec execute state program =
     match program with
-    | Skip (l) -> SS.singleton l
-    | Assign(l, _, _) -> SS.singleton l
-    | If (l, _, _, _) -> SS.singleton l
-    | While (l, _, _) -> SS.singleton l
+    | Skip (l) -> ()
+    | Assign (l, var, aexp) ->
+        Hashtbl.replace state var (Aexp.eval state aexp)
+    | Compose (c1, c2) ->
+        execute state c1;
+        execute state c2
+    | If (l, bexp, c1, c2) ->
+        (
+            match (Bexp.eval state bexp) with
+            | true  -> execute state c1
+            | false -> execute state c2
+        )
+    | While (l, bexp, c) as while_c ->
+        (
+            match (Bexp.eval state bexp) with
+            | true  ->
+                execute state c;
+                execute state while_c
+            | false -> ()
+        )
     ;;
+
--- src/main.ml
+++ src/main.ml
@@ -1,5 +1,5 @@
 (*
- * Fichier main
+ * main file
  *)
 
 let prog = Language.Compose(
@@ -19,4 +19,28 @@ let prog = Language.Compose(
         Language.Assign("5", "X", Aexp.Const(1)),
         Language.Assign("6", "X", Aexp.Plus(Aexp.Var("X"), Aexp.Const(1)))
     )
-)
+);;
+
+
+(*
+ * Modulo program
+ * Computes X mod Y
+ * Result is in X
+ *)
+let prog_modulo = Language.Compose(
+
+    (* if X <= 0 then X := -X *)
+
+    Language.If("1",
+        Bexp.Le(Aexp.Var("X"), Aexp.Const(0)),
+        Language.Assign("2", "X", Aexp.Minus(Aexp.Const(0), Aexp.Var("X"))),
+        Language.Skip("3")
+    ),
+
+    (* while X >= Y do X := X-Y *)
+
+    Language.While("4",
+        Bexp.Ge(Aexp.Var("X"), Aexp.Var("Y")),
+        Language.Assign("5", "X", Aexp.Minus(Aexp.Var("X"), Aexp.Var("Y")))
+    )
+);;
